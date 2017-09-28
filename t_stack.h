dbfa74146c67c24eef0de776ef06c568845dc5d3
sasessw <sasessw@gmail.com>
2017-09-28 03:46:17 +0300

    Update t_stack.h

--- t_stack.h
+++ t_stack.h
@@ -5,42 +5,46 @@
 #ifndef TEMPLATE_STACK_T_STACK_H
 #define TEMPLATE_STACK_T_STACK_H
 
+#include <iterator>
+
 template <typename T> class stack
 {
 public:
-    stack();
-    stack(size_t size);
-    size_t count() const;
-    void push(T const &);
+    stack<T>();
+    stack<T>(size_t size);
+    size_t count(T&&) const;
+    void push(T );
     T pop();
     ~stack();
-
+    stack(const stack<T>&) = delete;
+    stack& operator=(const stack<T>&) = delete;
 private:
     T * array_;
     size_t array_size_;
     size_t count_;
 };
 
-template <typename T> stack<T>::stack() : array_size_(10),count_(0) {
-    array_ = (T*)malloc(array_size_*sizeof(T));
-}
+template <typename T>
+stack<T>::stack() : array_size_(10),count_(0),array_(new T[10]){}
 
-template <typename T> stack<T>::stack(size_t size) : array_size_(size), count_(0) {
-    array_ = (T*)malloc(array_size_*sizeof(T));
-}
+template <typename T>
+stack<T>::stack(size_t size) : array_size_(size),count_(0),array_(new T[size]){}
 
-template <typename T> void stack<T>::push(T const & T_value) {
+template <typename T> void stack<T>::push(T T_value) {
     if (count_ == array_size_) {
+        T* newArray_ = new T[array_size_*2];
+        std::copy(array_, array_ + array_size_, newArray_);
+        delete[] array_;
+        array_ = newArray_;
         array_size_*=2;
-        array_ = (T*)realloc(array_, array_size_*sizeof(T));
     }
-        array_[count_] = T_value;
-        count_++;
+    array_[count_++] = std::move(T_value);
+
 }
 
 template <typename T> T stack<T>::pop() {
     if(count_ != 0) {
-        T tmp = array_[count_];
+        T tmp = std::move(array_[count_]);
 //        array_[count_] = NULL;
         --count_;
         return tmp;
@@ -50,7 +54,8 @@ template <typename T> T stack<T>::pop() {
 }
 
 template <typename T> stack<T>::~stack(){
-    free(array_);
+    delete[] array_;
 }
+
 
 #endif //TEMPLATE_STACK_T_STACK_H
